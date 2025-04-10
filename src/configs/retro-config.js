6b1e8cc58bf2a20ee3cefaa0604bbb8ec21927d4
Cory Trimm <cory.trimm@gmail.com>
2025-04-09 22:35:59 -0400

    Added a retro 90's theme

---
+++ src/configs/retro-config.js
@@ -0,0 +1,80 @@
+export default {
+  user: {
+    name: "Cory Trimm",
+    bio: "Serial builder of things - software, hardware, and everything in between. This is my little link in bio project built with Astro and Tailwind.",
+    profileImage: "/src/images/profilepic.png"
+  },
+  background: {
+    color: "#000000",
+    image: "",
+    pattern: "retro-grid"
+  },
+  theme: {
+    primary: "retro-yellow",
+    secondary: "retro-pink",
+    accent: "retro-cyan"
+  },
+  socialLinks: [
+    {
+      platform: "instagram",
+      url: "#",
+      color: "retro-pink"
+    },
+    {
+      platform: "threads",
+      url: "#",
+      color: "retro-cyan"
+    },
+    {
+      platform: "linkedin",
+      url: "#",
+      color: "retro-green"
+    },
+    {
+      platform: "spotify",
+      url: "#",
+      color: "retro-purple"
+    },
+    {
+      platform: "snapchat",
+      url: "#",
+      color: "retro-yellow"
+    },
+    {
+      platform: "tiktok",
+      url: "#",
+      color: "retro-orange"
+    }
+  ],
+  links: [
+    {
+      title: "Check Out My Blog",
+      url: "#",
+      icon: "newspaper",
+      color: "retro-green"
+    },
+    {
+      title: "Use This Template",
+      url: "#",
+      icon: "code",
+      color: "retro-orange"
+    },
+    {
+      title: "My Personal Web Site",
+      url: "#",
+      icon: "globe",
+      color: "retro-purple"
+    }
+  ],
+  products: [
+    {
+      title: "My YouTube Channel",
+      description: "Check out my latest videos on software development, hardware projects, and more!",
+      url: "#",
+      price: 0,
+      includePriceOnSite: false,
+      image: "/src/images/product1.png",
+      color: "retro-pink"
+    }
+  ]
+};
