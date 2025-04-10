a95596c43b9f8b7a7af7a322e770f880467df942
Cory Trimm <cory.trimm@gmail.com>
2025-04-09 21:56:36 -0400

    Added navbar theme changer + modern theme

---
+++ src/configs/modern-config.js
@@ -0,0 +1,64 @@
+export default {
+  user: {
+    name: "Cory Trimm",
+    bio: "Serial builder of things - software, hardware, and everything in between. This is my little link in bio project built with Astro and Tailwind.",
+    profileImage: "/src/images/profilepic.png"
+  },
+  background: {
+    color: "#f9fafb",
+    image: ""
+  },
+  socialLinks: [
+    {
+      platform: "instagram",
+      url: "#",
+    },
+    {
+      platform: "threads",
+      url: "#",
+    },
+    {
+      platform: "linkedin",
+      url: "#",
+    },
+    {
+      platform: "spotify",
+      url: "#",
+    },
+    {
+      platform: "snapchat",
+      url: "#",
+    },
+    {
+      platform: "tiktok",
+      url: "#",
+    }
+  ],
+  links: [
+    {
+      title: "Building a Modern Link in Bio with Astro",
+      url: "#",
+      icon: "newspaper"
+    },
+    {
+      title: "Use This Template",
+      url: "#",
+      icon: "code"
+    },
+    {
+      title: "My Personal Web Site",
+      url: "#",
+      icon: "globe"
+    }
+  ],
+  products: [
+    {
+      title: "My YouTube Channel",
+      description: "Check out my latest videos on software development, hardware projects, and more!",
+      url: "#",
+      price: 0,
+      includePriceOnSite: false,
+      image: "/src/images/product1.png"
+    }
+  ]
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: [
     "./src/**/*.{astro,js,jsx,ts,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       keyframes: {
